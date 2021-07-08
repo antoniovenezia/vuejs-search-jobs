4a7cbf4fd567e0f81a315a609346975befd7cefb
antoniovenezia <antoniovenezia00@gmail.com>
2021-07-08 09:36:14 +0200

    vuejs-search-jobs

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ new Vue(
 
             jobs: [
                 {
-                    id: 1,
+                    id: 10,
                     company: 'Perferendis',
                     position: 'BackEnd Developer',
                     description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
@@ -26,36 +26,85 @@ new Vue(
                     city: 'Milano',
                     contract: 'Part Time'
                 },
+                {
+                    id: 3,
+                    company: 'Boolean',
+                    position: 'FronteEnd Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '01/06/2021',
+                    logo: 'https://via.placeholder.com/100',
+                    city: 'Milano',
+                    contract: 'Part Time'
+                },
+                {
+                    id: 4,
+                    company: 'Boolean',
+                    position: 'FronteEnd Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '01/06/2021',
+                    logo: 'https://via.placeholder.com/100',
+                    city: 'Milano',
+                    contract: 'Part Time'
+                },
+                {
+                    id: 5,
+                    company: 'Boolean',
+                    position: 'FronteEnd Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '01/06/2021',
+                    logo: 'https://via.placeholder.com/100',
+                    city: 'Milano',
+                    contract: 'Part Time'
+                },
+                {
+                    id: 6,
+                    company: 'Boolean',
+                    position: 'FronteEnd Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '01/06/2021',
+                    logo: 'https://via.placeholder.com/100',
+                    city: 'Milano',
+                    contract: 'Part Time'
+                }
             ],
-            starred: [1],
+            starred: [1, 2, 3],
             applied: [],
+
+            listStarred: [],
+            listApplied: [],
 
         },
 
         methods: {
 
             classYellow: function (index) {
-                if (this.starred.includes(index + 1)) {
+                if (this.starred.includes(index) && !this.applied.includes(index)) {
                     return "yellow";
                 };
             },
 
             pushPreferiti: function (index) {
-                this.starred.push(index + 1);
-            },
-
-            pushApplied: function (index) {
-                this.applied.push(index + 1);
-            },
-
-            classRed: function (index) {
-                if (this.applied.includes(index + 1)) {
-                    return "red";
-                    
+                if (!this.starred.includes(index) && !this.applied.includes(index)) {
+                    this.starred.push(index);
+                    this.listStarred.push(this.jobs[index]);
                 };
             },
 
+            pushApplied: function (index) {
+                if (!this.applied.includes(index)) {
+                    this.applied.push(index);
+                    this.starred.splice(index);
+                };
+                this.listApplied.push(this.jobs[index]);
 
+            },
+
+            classRed: function (index) {
+                if (this.applied.includes(index)) {
+                    // this.starred.splice(index, 1);
+                    return "red";
+                };
+            },
         },
     });
 
